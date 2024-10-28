1d8dba328f92269980399a1e23abc5765a307b81
USTC-DingZhen <ycustc@mail.ustc.edu.cn>
2024-10-29 00:34:25 +0800

    update test

--- lexer.go
+++ lexer.go
@@ -57,11 +57,70 @@ func (t *token) equals(other *token) bool {
 
 type lexer func(string, cursor) (*token, cursor, error)
 
-//func lex(source string) ([]*token, error) {
-//	tokens := []*token{}
-//	cur := cursor{}
-//lex:
-//	for cur.pointer < uint(len(source)) {
-//		lexers := []lexer{lexKeword}
+//	func lex(source string) ([]*token, error) {
+//		tokens := []*token{}
+//		cur := cursor{}
+//
+// lex:
+//
+//		for cur.pointer < uint(len(source)) {
+//			lexers := []lexer{lexKeword}
+//		}
 //	}
-//}
+func lexNumeric(source string, ic cursor) (*token, cursor, bool) {
+	cur := ic
+	periodFound := false
+	expMarkerFound := false
+
+	for ; cur.pointer < uint(len(source)); cur.pointer++ {
+		c := source[cur.pointer]
+		cur.loc.col++
+		isDigit := c >= '0' && c <= '9'
+		isPeriod := c == '.'
+		isExpMarker := c == 'e'
+
+		if cur.pointer == ic.pointer {
+			if !isDigit && !isPeriod {
+				return nil, ic, false
+			}
+			periodFound = isPeriod
+			continue
+		}
+		if isPeriod {
+			//period comes only one
+			if periodFound {
+				return nil, ic, false
+			}
+			continue
+		}
+
+		if isExpMarker {
+			if expMarkerFound {
+				return nil, ic, false
+			}
+			periodFound = true
+			expMarkerFound = true
+
+			if cur.pointer == uint(len(source))-1 {
+				return nil, ic, false
+			}
+			cNext := source[cur.pointer+1]
+			if cNext == '-' || cNext == '+' {
+				cur.pointer++
+				cur.loc.col++
+			}
+			continue
+		}
+		if !isDigit {
+			break
+		}
+	}
+	if cur.pointer == ic.pointer {
+		return nil, ic, false
+	}
+	return &token{
+		value: source[ic.pointer:cur.pointer],
+		loc:   ic.loc,
+		kind:  numericKind,
+	}, cur, true
+}
